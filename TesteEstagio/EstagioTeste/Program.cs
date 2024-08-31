873260fa3f107aa624e5d2cec0bc71d762f417f8
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-08-31 12:46:38 -0300

    Feat: Resolucao de teste 2

--- TesteEstagio/EstagioTeste/Program.cs
+++ TesteEstagio/EstagioTeste/Program.cs
@@ -3,11 +3,14 @@ using EstagioTeste;
 
 Console.WriteLine("Teste Estagio Target");
 
-
-Console.WriteLine("Informe o numero para verificação");
+// teste 01 Sequencia Fibonacci
+Console.WriteLine("\nInforme o numero para verificação se pertence a sequencia de fibonacci");
 Teste01.PertenceSequenciaFibonacci(Console.ReadLine());
 
 
+// teste 02 verificação letra a ou A
+Console.WriteLine("\nInforme o dado para verificação se contem letra a ou A");
+Teste2.ContemLetraA(Console.ReadLine());
 
 
 
---
+++ TesteEstagio/EstagioTeste/Teste2.cs
@@ -0,0 +1,22 @@
+﻿namespace EstagioTeste
+{
+    public static class Teste2
+    {
+        public static void ContemLetraA(string dadoEntrada)
+        {
+            
+            string valorRecebido = dadoEntrada;
+           
+            char[] arrayChar = valorRecebido.ToCharArray();
+
+            if (arrayChar.Contains('a') == true || arrayChar.Contains('A') == true)
+            {
+                Console.WriteLine($"O {valorRecebido} contem a letra a/A");   
+            }
+            else
+            {
+                Console.WriteLine($"O {valorRecebido} não contem a letra a/A");
+            }
+        }
+    }
+}
