0ee90baae95a72018d596b57305a69692bb33acc
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-08-31 12:20:46 -0300

    Feat: Resolucao de teste 01

---
+++ TesteEstagio/EstagioTeste/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EstagioTeste;
+
+Console.WriteLine("Teste Estagio Target");
+
+
+Console.WriteLine("Informe o numero para verificação");
+Teste01.PertenceSequenciaFibonacci(Console.ReadLine());
+
+
+
+
+
+
+
+
---
+++ TesteEstagio/EstagioTeste/Teste01.cs
@@ -0,0 +1,51 @@
+﻿
+namespace EstagioTeste
+{
+    public static class Teste01
+    {
+        public static void PertenceSequenciaFibonacci(string dadoEntrada)
+        {
+            int numeroEntrada = int.Parse(dadoEntrada);
+            bool resposta = VerificaFibonacci(numeroEntrada);
+            
+
+            if (resposta == true)
+            {
+                string respostaFinal = $"O número {numeroEntrada} pertence à sequência de Fibonacci.";
+                Console.WriteLine(respostaFinal);
+                return ;
+            }
+            else
+            {
+                string respostaFinal = $"O número {numeroEntrada} não pertence à sequência de Fibonacci.";
+                Console.WriteLine(respostaFinal);
+                return ;
+            }
+        }
+
+
+
+        public static bool VerificaFibonacci(int numeroEntrada)
+        {
+            if (numeroEntrada < 0)
+            {
+                return false;
+            }
+
+            int n1 = 0;
+            int n2 = 1;
+
+            while(n1 <= numeroEntrada)
+            {
+                if (n1 == numeroEntrada)
+                {
+                    return true;
+                }
+                int proximoNumero = n1 + n2;
+                n1 = n2;
+                n2 = proximoNumero;
+            }
+            return false;
+        }
+    }
+}
